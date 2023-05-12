f00d9b122b31fbf98e696902b498ede9d94dd6bc
jinan <ja.choi@sweetk.co.kr>
2023-05-13 00:47:27 +0900

    1. cell과 controller 기능을 담당하는 클래스를 만들고 스토리보드에서 그 이름으로 class를 연결해줌 2. 스토리보드 안에서 컬렉션 뷰 내부의 라벨들을 세팅해줌

---
+++ Diary/StarCell.swift
@@ -0,0 +1,12 @@
+//
+//  StarCell.swift
+//  Diary
+//
+//  Created by 최진안 on 2023/05/13.
+//
+
+import UIKit
+
+class StarCell: UICollectionViewCell {
+    
+}
---
+++ Diary/StarViewController.swift
@@ -0,0 +1,25 @@
+//
+//  StarViewController.swift
+//  Diary
+//
+//  Created by 최진안 on 2023/05/13.
+//
+
+import UIKit
+
+class StarViewController: UIViewController {
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+    
+    
+    
+    
+
+}
