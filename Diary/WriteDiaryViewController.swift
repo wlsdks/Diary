c57c8b0d9f2eea0bf8cff23f940298c94ebc6f3e
jinan <ja.choi@sweetk.co.kr>
2023-05-13 14:22:12 +0900

    1. storyboard에서 다이어리를 추가할때 나오는 ui 추가 -> view를 생성해서 그 안에 만들어주고 tab bar item으로 눌러서 들어가게 하고 segue는 push로 설정해줌 2. swift파일로 컨트롤러를 만들어서 storyboard에서 연결시켜줌

---
+++ Diary/WriteDiaryViewController.swift
@@ -0,0 +1,19 @@
+//
+//  WriteDiaryViewController.swift
+//  Diary
+//
+//  Created by 최진안 on 2023/05/13.
+//
+
+import UIKit
+
+class WriteDiaryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+    
+
+}
