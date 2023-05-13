821521533e969ebbc65b58e400abe08cb6f48d5c
jinan <ja.choi@sweetk.co.kr>
2023-05-13 18:02:44 +0900

    1. 즐겨찾기를 하면 하단 탭에서 즐겨찾기로 이동시 즐겨찾기한 내용이 표시되도록 코드 작성

--- Diary/controller/StarViewController.swift
+++ Diary/controller/StarViewController.swift
@@ -10,16 +10,77 @@ import UIKit
 class StarViewController: UIViewController {
     
     
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    private var diaryList = [Diary]()
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        configureCollectionView()
+        
+    }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.loadStarDiaryList()
+    }
+    
+    private func configureCollectionView() {
+        self.collectionView.collectionViewLayout = UICollectionViewFlowLayout()
+        self.collectionView.contentInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
+    }
+    
+    // 즐겨찾기 적용 일기 데이터 로드 - 고차함수 적용
+    private func loadStarDiaryList() {
+        // userDefaults에 접근한다.
+        let userDefaults = UserDefaults.standard
+        guard let data = userDefaults.object(forKey: "diaryList") as? [[String: Any]] else { return }
+        self.diaryList = data.compactMap {
+            guard let title = $0["title"] as? String else { return nil }
+            guard let contents = $0["contents"] as? String else { return nil }
+            guard let date = $0["date"] as? Date else { return nil }
+            guard let isStar = $0["isStar"] as? Bool else { return nil }
+            return Diary(title: title, contents: contents, date: date, isStar: isStar)
+        }.filter({
+            $0.isStar == true
+        }).sorted(by: {
+            $0.date.compare($1.date) == .orderedDescending
+        })
+        self.collectionView.reloadData()
     }
     
     
+    // MARK: - 데이터 타입을 전달받으면 문자열로 변환시켜주는 메서드 선언
+    private func dateToString(date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yy년 MM월 dd일(EEEEE)"
+        formatter.locale = Locale(identifier: "ko_KR") // 한국어로 표시되게함
+        return formatter.string(from: date)
+    }
     
-    
+}
 
+extension StarViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.diaryList.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "StarCell", for: indexPath) as? StarCell else { return UICollectionViewCell() }
+        let diary = self.diaryList[indexPath.row]
+        cell.titleLabel.text = diary.title
+        cell.dateLabel.text = self.dateToString(date: diary.date)
+        return cell
+    }
+    
+}
+
+extension StarViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: UIScreen.main.bounds.width - 20, height: 80)
+    }
 }
--- Diary/models/StarCell.swift
+++ Diary/models/StarCell.swift
@@ -9,4 +9,8 @@ import UIKit
 
 class StarCell: UICollectionViewCell {
     
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    
 }
