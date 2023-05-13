d1acd6687fccca9a46add7c0c0ecce77483ae40e
jinan <ja.choi@sweetk.co.kr>
2023-05-13 16:22:58 +0900

    1. DiaryDetailViewController에 outlet변수와 action 함수 선언

--- Diary/controller/DiaryDetailViewController.swift
+++ Diary/controller/DiaryDetailViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class DiaryDetailViewController: UIViewController {
 
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var contentsTextView: UITextView!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
 
     }
     
-
+    @IBAction func tabEditButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func tapDeleteButton(_ sender: UIButton) {
+    }
+    
 }
